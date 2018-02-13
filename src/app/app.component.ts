712155d9e05037d208a16bca0c6de993d822e85a
tatomyr <andrew.tatomyr@gmail.com>
2018-02-13 15:33:35 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private myErrors: String = '';
+  private myForm: FormGroup;
+  constructor( ) {
+    this.myForm = new FormGroup({
+      myInput: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[A-Z].+/),
+      ])
+    });
+  }
+  hello = () => {
+    document.querySelector('#modal').showModal();
+  };
+  close = () => {
+    document.querySelector('#modal').close();
+  };
+  keyClick(){
+    console.log(  this.myForm.controls.myInput);
+    this.myErrors = this.myForm.controls.myInput.errors ? 'Invalid' : ''
+  }
 }
